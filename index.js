eb33c3fa87febac3d5fcab8c74dbd9b42bc47d7c
Naufal Wijaya <99630284+naufalw0501@users.noreply.github.com>
2023-08-07 23:47:32 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+// import Power from './Power';
+
+AppRegistry.registerComponent(appName, () => App);
